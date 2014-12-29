c86add7be8b5a5bfb8eba9657407ded3e3b821b9
Elliot Chance <elliotchance@gmail.com>
2014-12-30 00:42:49 +1100

    testFetchingANegativeIndexThrowsAnException

--- tests/Elliotchance/Iterator/PagedIteratorTest.php
+++ tests/Elliotchance/Iterator/PagedIteratorTest.php
@@ -2,14 +2,33 @@
 
 namespace Elliotchance\Iterator;
 
+use ArrayAccess;
 use Concise\TestCase;
 use Countable;
+use OutOfBoundsException;
 
-class PagedIterator1 implements Countable
+class PagedIterator1 implements Countable, ArrayAccess
 {
     public function count()
     {
         return 0;
+    }
+
+    public function offsetExists($offset)
+    {
+    }
+
+    public function offsetGet($offset)
+    {
+        throw new OutOfBoundsException("Index out of bounds: -1");
+    }
+
+    public function offsetSet($offset, $value)
+    {
+    }
+
+    public function offsetUnset($offset)
+    {
     }
 }
 
@@ -19,5 +38,15 @@ class PagedIteratorTest extends TestCase
     {
         $iterator = new PagedIterator1();
         $this->assert(count($iterator), equals, 0);
+    }
+
+    /**
+     * @expectedException \OutOfBoundsException
+     * @expectedExceptionMessage Index out of bounds: -1
+     */
+    public function testFetchingANegativeIndexThrowsAnException()
+    {
+        $iterator = new PagedIterator1();
+        $iterator[-1];
     }
 }
